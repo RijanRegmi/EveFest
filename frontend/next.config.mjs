a3ef23be152b4494710c8ea3f60deae10977d4e3
Rijan Regmi <rijanregmi111@gmail.com>
2026-07-28 18:44:23 +0545

    update packages

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname, "../"),
+  },
   images: {
     remotePatterns: [
       {
@@ -21,16 +30,6 @@ const nextConfig = {
         hostname: "**",
       },
     ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: process.env.BACKEND_API_URL
-          ? `${process.env.BACKEND_API_URL}/api/:path*`
-          : "http://localhost:5000/api/:path*",
-      },
-    ];
   },
 };
 
